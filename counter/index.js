c355a1b0b19f064f97a083745eb7c268bae2e637
zennon-sml <zennonsampaioml@gmail.com>
2025-10-21 11:28:34 -0300

    feat: counter saving a sets to zero

--- counter/index.js
+++ counter/index.js
@@ -1,2 +1,16 @@
-// document.getElementById("count").innerText = 5
-let count = 0
+let count = 0;
+let countEl = document.getElementById("count");
+let saveEL = document.getElementById("save")
+
+
+function increment() {
+    count += 1;
+    console.log(count);
+    countEl.innerText = count;
+};
+
+function save() {
+    saveEL.innerText += " - " + count
+    count = 0
+    countEl.innerText = count;
+};
